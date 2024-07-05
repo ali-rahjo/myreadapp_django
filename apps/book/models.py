1543b19ca97140b53e22a63c087c8e16eca65912
ali-rahjo <alirahjo104@gmail.com>
2024-07-05 19:48:53 +0200

    added some features

--- apps/book/models.py
+++ apps/book/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField # postgresql specific
 
 # Create your models here.
+class BookManager(models.Manager):
+     def get_books_by_tags(self, *tags):
+          # self = Book.objects
+          return self.filter(tags__name__in=tags)
+     
+
 
 class Tag(models.Model):
      name = models.CharField(max_length=50, unique=True)
@@ -58,12 +64,30 @@ class Book(models.Model):
     book_format= models.CharField(max_length=2, choices=BOOK_FORMAT, default='eb')
     tags = models.ManyToManyField('book.Tag', related_name='book')
 
+    # override objects with now manager
+
+    objects = BookManager()
+
+    def get_books_by_tags(cls, *tags):
+         return cls.objects.filter(tags__name__in=tags)
+
     def __str__(self) -> str:
         return f'{self.title}({self.isbn})'
-
-
+    
+    # @classmethod
+    # def get_books_by_tags(cls, *tags):
+    #      return cls.objects.filter(tags__name__in=tags)
 # author = Author(first_name='doris', last_name='kelly')
 # author.save()
 # book = Book.objects.first()
 # book.author.add(author, through_defaults={'role': 'author'})
+
+    @property
+    def short_des(self):
+        return f'{self.description[0:30]}...'
+    
+    def __str__(self) -> str:
+         return f'{self.title}({self.isbn})'
+
+
 
--- apps/reader/models.py
+++ apps/reader/models.py
@@ -1,6 +1,26 @@
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import  AbstractUser, User
 # Create your models here.
+
+class NIC(models.Model):
+    nic_number = models.CharField(max_length=10, primary_key=True)
+    delivery_date = models.DateField()
+    # auto calcolated field from other field
+    #expiration_date = models.DateField()
+
+    expiration_date = models.GeneratedField(
+        expression = models.F('delivery_date') + timedelta(days=51827),
+        output_field = models.DateField(),
+        db_persist = True
+    )
+
+    def __str__(self) -> str:
+        return f'{self.nic_number}(del: {self.delivery_date}, exp: {self.expiration_date})'
+
+    #NIC.objects.filter(delivery_date= models.F('expiration_date'))
+    
+
 # Reader -> AbstractUser -> AbstractBaseUser -> models.Model
 class Reader(AbstractUser):
     READER_TITLE = {
@@ -20,7 +40,13 @@ class Reader(AbstractUser):
     title = models.CharField(max_length=5, null=True, blank=True, choices=READER_TITLE)
     # If you want to remove the default, just set it to None
     # email = None
-
+    # reader_set
+    nic = models.OneToOneField(
+        'reader.NIC', related_name='reader', 
+        on_delete=models.CASCADE,
+        null = True,
+        blank = True)
+    
     class Meta:
         # Define some meta data, including constraints
         constraints = [
